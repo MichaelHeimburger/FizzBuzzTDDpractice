43bb4deb467ea6d4f326b15add2d5b70e50ce948
MichaelHeimburger <mrheimburger45@gmail.com>
2018-05-31 22:29:21 -0400

    first commit

--- FizzBuzz.Test/FizzBuzzerTests.cs
+++ FizzBuzz.Test/FizzBuzzerTests.cs
@@ -20,5 +20,79 @@ namespace FizzBuzz.Test
             Assert.That(result, Is.EqualTo("1"));
             //should come up with no build errors but a failed test, 
         }
+        [Test]
+        public void Get_Returns_2_for_2()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(2);
+
+            Assert.That(result, Is.EqualTo("2"));
+        }
+        [Test]
+        public void Get_Returns_Fizz_for_3()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+        [Test]
+        public void for_mul_3()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(6);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+        [Test]
+        public void Get_Returns_Fizz_for_4()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(4);
+
+            Assert.That(result, Is.EqualTo("4"));
+        }
+        [Test]
+        public void Get_Returns_Fizz_for_5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(5);
+            // added if for == 5
+            Assert.That(result, Is.EqualTo("Buzz"));
+
+        }
+        [Test]
+        public void for_mul_5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(10);
+            // changed if to mod 5 ==
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+        [Test]
+        public void for_mul_3and5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(15);
+            // changed if to mod 5 ==
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+        [Test]
+        public void for_mul_3and52()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(30);
+            // changed if to mod 5 ==
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
     }
 }
--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -10,7 +10,32 @@ namespace FizzBuzz
     {
         public string Get(int num)
         {
-            return "1";
+            if ((IsMultipleOf3(num))&&(IsMultipleOf5(num)))
+            {
+                return "FizzBuzz";
+            }
+            if (IsMultipleOf3(num))
+            {
+                return "Fizz";
+            }
+            if (IsMultipleOf5(num))
+            {
+                return "Buzz";
+            }
+            return num.ToString();
+            
+
+        }
+
+
+        private bool IsMultipleOf3(int numberToCheck)
+        { // refactored code doesnt modify the function but instead adds a method that does the math operation instead of having it in the main code
+            return numberToCheck % 3 == 0;
+        }
+        private bool IsMultipleOf5(int numberToCheck)
+        { // refactored code doesnt modify the function but instead adds a method that does the math operation instead of having it in the main code
+            return numberToCheck % 5 == 0;
         }
     }
+
 }
